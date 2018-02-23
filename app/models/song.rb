7985bce3af790b8405b68d293e70f00db24be390
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-02-23 20:06:50 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,6 +4,7 @@ class Song < ActiveRecord::Base
 
   with_options if: :released do |song|
     song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
   end
 
 end
