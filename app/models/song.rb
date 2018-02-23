4bf391e63589b1473d071a216ec2d657d3e41425
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-02-23 19:29:03 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :title, uniqueness: true
 
-  with options if: :released do |song|
+  with_options if: :released do |song|
     song.validates :release_year, presence: true
   end
 
