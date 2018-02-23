e322c8dbb7c78a7d4270b21f991c2254bef5ec3e
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-02-23 20:52:45 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,11 +13,11 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params(:title, :released, :release_year, :artist_name, :genre))
-      if song.valid?
-        song.save
+      if @song.valid?
+        @song.save
         redirect_to song_path(@song)
-      else
-        render :new
+      else 
+        render :new 
       end
   end
 
